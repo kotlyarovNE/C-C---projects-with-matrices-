31f81aa874a67ffaec6643c27a23c8b0ee056f08
kotlyarovNE <71189788+kotlyarovNE@users.noreply.github.com>
2021-08-25 17:56:51 +0300

    Add files via upload

---
+++ solution of the system using MPI/input.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "functions.h"
+#include <mpi.h>
+#include<math.h>
+
+void input(int k, int n, double *A, double *B, int my_rank, int p) {
+	int i = 0, j = 0, rows;
+    double x;
+
+	if (my_rank + 1 > n%p) rows = n/p;
+	else rows = n/p + 1;
+
+	for (i = 0; i < rows; i++)
+	{
+		for (x = 0.0, j = 0; j < n; j++){
+            A[i * n + j] = f(k, n, my_rank + p * i, j);
+			if ((j % 2) == 0) x += A[i * n + j];
+		}
+		B[i] = x;
+	}
+}
+
+
+void input_from_file(int n, double *A, double *a, double *b, int my_rank, int p){
+    int i,j;
+    int rows ;
+    double x;
+
+    if (my_rank + 1 > n%p) rows = n/p;
+	else rows = n/p + 1;
+
+	for (i = 0; i < rows; i++)
+	{
+		for (x = 0.0, j = 0; j < n; j++)
+		{
+			a[i * n + j] = A[(my_rank + p * i) * n + j];
+			if ((j % 2) == 0) x += a[i * n + j];
+		}
+		b[i] = x;
+	}
+}
+
+
+
+
+
+
+double f(int k, int n, int i, int j)
+{
+	if (k == 1) return (n - max(i+1, j+1) +1);
+	if (k == 2) return (max(i+1, j+1)) ;
+	if (k == 3) return max(i-j, j-i);
+	if (k == 4) return 1.0/(i+j+1);
+	else return 0;
+}
+
+
+
+
+int matr_file(int n, char *filename, double *A){
+	FILE *IN ;
+
+	IN = fopen(filename, "r");
+	if (IN == NULL)
+		return -1;
+
+    for (int i = 0; i < n * n; i++) {
+		if (fscanf(IN, "%lf", &A[i]) != 1) {
+			fclose(IN);
+			return -2;
+		}
+	}
+
+	fclose(IN);
+	return 0;
+}
+
+
+
+int max(int x, int y) {
+	if ( x > y ) return x;
+	else return y;
+}
+
+
+
+
+
+void B_Matr(double *a, double*b, int n, int p, int my_rank){
+int i, j, first_row, last_row;
+double x;
+
+    first_row = n * my_rank;
+	first_row /= p;
+
+	last_row = n * (my_rank + 1);
+	last_row = last_row / p - 1;
+
+for (i = 0; i < n; i++)
+	{
+		for (x = 0.0, j = 0; j < n; j++){
+			if ((j % 2) == 0) x += a[i * n + j];
+		}
+		b[i] = x;
+	}
+}
+
+
+void inp_A_B(int n, int k, double *A, double *B){
+int i,j;
+double tmp;
+
+    for(i=0;i < n; i++){
+                for(j=0; j < n; j++){
+                    A[i*n+j] = f(k,n,i,j);
+                }
+        }
+
+    for (i = 0; i < n; ++i)
+            {
+                tmp = 0.0;
+                for (j = 0; j < n; ++j)
+                {
+                    if (j % 2 == 0)
+                    tmp += A[i * n + j];
+                }
+                B[i] = tmp;
+            }
+
+}
+
+void input_b_after_solution(double *B, double *b, int n, int my_rank, int p, double *buf){
+int i;
+double x[1];
+MPI_Status status;
+
+
+    for (i = 0; i < n; i++)
+	{
+		if (my_rank == 0)
+		{
+			if (my_rank == i%p){
+
+                    B[i] = b[i/p];
+            }
+			else
+			{
+				MPI_Recv(x, 1, MPI_DOUBLE, i%p, 0, MPI_COMM_WORLD, &status);
+                B[i] = x[0];
+			}
+		}
+		else if (my_rank == i%p)
+		{
+			x[0] = b[i/p];
+			MPI_Send(x, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		}
+	}
+
+
+
+	for (int k = 1; k < p; k++){
+	    if(my_rank == 0){
+            MPI_Send(B, n, MPI_DOUBLE, k, 0, MPI_COMM_WORLD);
+        }
+
+        if(my_rank == k){
+            MPI_Recv(buf, n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+            for(i = 0;i < n; i++)
+                B[i] = buf[i];
+        }
+	}
+}
+
+
+
---
+++ solution of the system using MPI/nevazka.c
@@ -0,0 +1,139 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#include "functions.h"
+#include <math.h>
+
+double Error(int n, double* a, double *B, double *vect, int my_rank, int p) //Норма невязки, запотел...
+{
+	int i,j;
+	double rezult1 = 0; //||Ax - b||
+	double rezult2 = 0.0; //||B||
+	double tmp = 0, x[1], y[1];
+	double Nev_error = 0;
+	MPI_Status status;
+
+    for (i = 0; i < n; i++)
+	{
+		if (my_rank == 0)
+		{
+			if (my_rank == i%p){
+                tmp = 0.0;
+                for(j = 0; j < n; j++){
+                    tmp += a[i/p * n + j] * B[j];}
+                tmp -= vect[i/p];
+                rezult1 += tmp * tmp;
+            }
+			else
+			{
+				MPI_Recv(x, 1, MPI_DOUBLE, i%p, 0, MPI_COMM_WORLD, &status);
+                rezult1 += x[0];
+			}
+		}
+		else if (my_rank == i%p)
+		{
+		    tmp = 0.0;
+		    for(j = 0; j < n; j++){
+                    tmp += a[i/p * n + j] * B[j];}
+            tmp -= vect[i/p];
+            x[0] = tmp * tmp;
+			MPI_Send(x, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
+	//Теперь посчитаем норму b:
+
+	for (i = 0; i < n; i++)
+	{
+		if (my_rank == 0)
+		{
+			if (my_rank == i%p){
+                rezult2 += vect[i/p]*vect[i/p];
+            }
+			else
+			{
+				MPI_Recv(y, 1, MPI_DOUBLE, i%p, 0, MPI_COMM_WORLD, &status);
+                rezult2 += y[0];
+			}
+		}
+		else if (my_rank == i%p)
+		{
+			y[0] = vect[i/p]*vect[i/p];
+			MPI_Send(y, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		}
+	}
+
+
+
+	if(my_rank == 0)
+    {
+        Nev_error = sqrt(rezult1/rezult2);
+        printf("%10.3e\n ", Nev_error);
+	}
+
+
+return Nev_error;
+}
+
+
+double accur(int n, double* x, int my_rank)
+{
+	double tmp;
+	int i;
+	double rez;
+
+	rez = 0.0;
+	for (i = 0; i < n; i++)
+	{
+		tmp = x[i];
+
+		if (i % 2 == 0)
+			tmp -= 1.0;
+
+		rez +=tmp*tmp;
+	}
+
+	if(my_rank == 0)
+        printf("%lf ", sqrt(rez));
+
+	return sqrt(rez);
+}
+
+
+void NORM(int n, double *a, double *b, int p, int my_rank){
+int i, j, rows;
+double NORM = 0, tmp = 0, final_norm = 0;
+
+    if (my_rank + 1 > n%p) rows = n/p;
+	else rows = n/p + 1;
+
+	for(i = 0; i < n; i++)
+        NORM += fabs(a[i]);
+
+
+	for(i = 1; i < rows; i++){
+        tmp = 0;
+        for(j = 0; j < n; j++){
+            tmp += fabs(a[i * rows + j]);
+        }
+        if(tmp > NORM)
+            NORM = tmp;
+	}
+
+	MPI_Allreduce(&NORM, &final_norm, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    if(final_norm < 1e-13){
+        for(i = 0; i < n; i++){
+            if(my_rank == i%p){
+                for(j = 0; j < n; j++){
+                    a[i/p * n + j] /= final_norm;
+                }
+                b[i/p] /= final_norm;
+            }
+        }
+    }
+
+}
